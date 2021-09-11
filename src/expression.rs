3ba291afb18f3eff06c434f67b32cadb3d97c179
Ungureanu - Vranceanu, George <george.ungureanu---vranceanu19@imperial.ac.uk>
2021-09-11 09:50:29 +0300

    Finalize expression parser (no negative numbers)

--- src/expression.rs
+++ src/expression.rs
@@ -2,24 +2,28 @@ use nom::{
     branch::alt,
     bytes::complete::{tag, take_while},
     character::{
-        complete::{digit1 as digit, space0 as space},
+        complete::{char, digit1 as digit, space0 as space},
         is_alphabetic, is_alphanumeric,
     },
     combinator::{fail, map, map_res},
-    sequence::delimited,
+    multi::fold_many0,
+    sequence::{delimited, pair},
     IResult,
 };
-use std::{num::ParseIntError, str::FromStr};
+use std::num::ParseIntError;
 
 #[derive(PartialEq, Clone, Debug)]
 pub enum Expression<'a> {
     Constant(i32),
     Variable(&'a str),
-    Bracket(Box<Expression<'a>>),
+    Multiplication(Box<Expression<'a>>, Box<Expression<'a>>),
+    Division(Box<Expression<'a>>, Box<Expression<'a>>),
+    Addition(Box<Expression<'a>>, Box<Expression<'a>>),
+    Subtraction(Box<Expression<'a>>, Box<Expression<'a>>),
 }
 
 fn parens(i: &str) -> IResult<&str, Expression> {
-    delimited(space, delimited(tag("("), factor, tag(")")), space)(i)
+    delimited(space, delimited(tag("("), expr, tag(")")), space)(i)
 }
 
 fn factor(i: &str) -> IResult<&str, Expression> {
@@ -42,40 +46,37 @@ fn id(i: &str) -> IResult<&str, &str> {
     take_while(|c| is_alphanumeric(c as u8) || (c as char == '_'))(i)
 }
 
-// // We read an initial factor and for each time we find
-// // a * or / operator followed by another factor, we do
-// // the math by folding everything
-// fn term(i: &str) -> IResult<&str, Expression> {
-//     let (i, init) = factor(i)?;
+fn term(i: &str) -> IResult<&str, Expression> {
+    let (i, init) = factor(i)?;
 
-//     fold_many0(
-//         pair(alt((char('*'), char('/'))), factor),
-//         move || init,
-//         |acc, (op, val): (char, i64)| {
-//             if op == '*' {
-//                 acc * val
-//             } else {
-//                 acc / val
-//             }
-//         },
-//     )(i)
-// }
+    fold_many0(
+        pair(alt((char('*'), char('/'))), factor),
+        move || init.clone(),
+        |acc, (op, expr)| {
+            if op == '*' {
+                Expression::Multiplication(Box::new(acc), Box::new(expr))
+            } else {
+                Expression::Division(Box::new(acc), Box::new(expr))
+            }
+        },
+    )(i)
+}
 
-// pub fn expr(i: &str) -> IResult<&str, Expression> {
-//     let (i, init) = term(i)?;
+pub fn expr(i: &str) -> IResult<&str, Expression> {
+    let (i, init) = term(i)?;
 
-//     fold_many0(
-//         pair(alt((char('+'), char('-'))), term),
-//         move || init,
-//         |acc, (op, val): (char, i64)| {
-//             if op == '+' {
-//                 acc + val
-//             } else {
-//                 acc - val
-//             }
-//         },
-//     )(i)
-// }
+    fold_many0(
+        pair(alt((char('+'), char('-'))), term),
+        move || init.clone(),
+        |acc, (op, expr)| {
+            if op == '+' {
+                Expression::Addition(Box::new(acc), Box::new(expr))
+            } else {
+                Expression::Subtraction(Box::new(acc), Box::new(expr))
+            }
+        },
+    )(i)
+}
 
 #[cfg(test)]
 mod test {
@@ -95,24 +96,155 @@ mod test {
         );
     }
 
-    // #[test]
-    // fn term_test() {
-    //     assert_eq!(term(" 12 *2 /  3"), Ok(("", 8)));
-    //     assert_eq!(term(" 2* 3  *2 *2 /  3"), Ok(("", 8)));
-    //     assert_eq!(term(" 48 /  3/2"), Ok(("", 8)));
-    // }
+    #[test]
+    fn term_test() {
+        assert_eq!(
+            term(" 12 *2 /  3"),
+            Ok((
+                "",
+                Expression::Division(
+                    Box::new(Expression::Multiplication(
+                        Box::new(Expression::Constant(12)),
+                        Box::new(Expression::Constant(2))
+                    )),
+                    Box::new(Expression::Constant(3)),
+                )
+            ))
+        );
 
-    // #[test]
-    // fn expr_test() {
-    //     assert_eq!(expr(" 1 +  2 "), Ok(("", 3)));
-    //     assert_eq!(expr(" 12 + 6 - 4+  3"), Ok(("", 17)));
-    //     assert_eq!(expr(" 1 + 2*3 + 4"), Ok(("", 11)));
-    // }
+        assert_eq!(
+            term(" 2* 3  *2 *2 /  3"),
+            Ok((
+                "",
+                Expression::Division(
+                    Box::new(Expression::Multiplication(
+                        Box::new(Expression::Multiplication(
+                            Box::new(Expression::Multiplication(
+                                Box::new(Expression::Constant(2)),
+                                Box::new(Expression::Constant(3))
+                            )),
+                            Box::new(Expression::Constant(2))
+                        )),
+                        Box::new(Expression::Constant(2))
+                    )),
+                    Box::new(Expression::Constant(3)),
+                )
+            ))
+        );
+        assert_eq!(
+            term(" 48 /  3/2"),
+            Ok((
+                "",
+                Expression::Division(
+                    Box::new(Expression::Division(
+                        Box::new(Expression::Constant(48)),
+                        Box::new(Expression::Constant(3))
+                    )),
+                    Box::new(Expression::Constant(2)),
+                )
+            ))
+        );
+    }
 
-    // #[test]
-    // fn parens_test() {
-    //     assert_eq!(expr(" (  2 )"), Ok(("", 2)));
-    //     assert_eq!(expr(" 2* (  3 + 4 ) "), Ok(("", 14)));
-    //     assert_eq!(expr("  2*2 / ( 5 - 1) + 3"), Ok(("", 4)));
-    // }
+    #[test]
+    fn expr_test() {
+        assert_eq!(
+            expr(" 1 +  2 "),
+            Ok((
+                "",
+                Expression::Addition(
+                    Box::new(Expression::Constant(1)),
+                    Box::new(Expression::Constant(2)),
+                )
+            ))
+        );
+        assert_eq!(
+            expr(" 12 + 6 - 4+  3"),
+            Ok((
+                "",
+                Expression::Addition(
+                    Box::new(Expression::Subtraction(
+                        Box::new(Expression::Addition(
+                            Box::new(Expression::Constant(12)),
+                            Box::new(Expression::Constant(6))
+                        )),
+                        Box::new(Expression::Constant(4))
+                    )),
+                    Box::new(Expression::Constant(3)),
+                )
+            ))
+        );
+        assert_eq!(
+            expr(" 1 + 2*3 + 4"),
+            Ok((
+                "",
+                Expression::Addition(
+                    Box::new(Expression::Addition(
+                        Box::new(Expression::Constant(1)),
+                        Box::new(Expression::Multiplication(
+                            Box::new(Expression::Constant(2)),
+                            Box::new(Expression::Constant(3))
+                        )),
+                    ),),
+                    Box::new(Expression::Constant(4)),
+                )
+            ))
+        );
+    }
+
+    #[test]
+    fn parens_test() {
+        assert_eq!(expr(" (  2 )"), Ok(("", Expression::Constant(2))));
+        assert_eq!(
+            expr(" 2* (  3 + 4 ) "),
+            Ok((
+                "",
+                Expression::Multiplication(
+                    Box::new(Expression::Constant(2)),
+                    Box::new(Expression::Addition(
+                        Box::new(Expression::Constant(3)),
+                        Box::new(Expression::Constant(4))
+                    ))
+                )
+            ))
+        );
+        assert_eq!(
+            expr("  2*2 / ( 5 - 1) + 3"),
+            Ok((
+                "",
+                Expression::Addition(
+                    Box::new(Expression::Division(
+                        Box::new(Expression::Multiplication(
+                            Box::new(Expression::Constant(2)),
+                            Box::new(Expression::Constant(2))
+                        )),
+                        Box::new(Expression::Subtraction(
+                            Box::new(Expression::Constant(5)),
+                            Box::new(Expression::Constant(1))
+                        )),
+                    )),
+                    Box::new(Expression::Constant(3))
+                )
+            ))
+        );
+        assert_eq!(
+            expr("  var - 2 / ( 5 - c) + 3"),
+            Ok((
+                "",
+                Expression::Addition(
+                    Box::new(Expression::Subtraction(
+                        Box::new(Expression::Variable("var")),
+                        Box::new(Expression::Division(
+                            Box::new(Expression::Constant(2)),
+                            Box::new(Expression::Subtraction(
+                                Box::new(Expression::Constant(5)),
+                                Box::new(Expression::Variable("c"))
+                            )),
+                        )),
+                    )),
+                    Box::new(Expression::Constant(3))
+                )
+            ))
+        );
+    }
 }
